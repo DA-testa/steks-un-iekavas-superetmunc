e2b9002034dc56a955c2de65b626f69d58c9e98d
Edmunds <93928858+superetmunc@users.noreply.github.com>
2023-02-17 11:21:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
-import os
-from collections import namedtuple
+def is_balanced(s):
+    stack = []
+    opening = set('({[')
+    pairs = set([ ('(',')'), ('{','}'), ('[',']') ])
+    for char in s:
+        if char in opening:
+            stack.append(char)
+        else:
+            if len(stack) == 0:
+                return False
+            last_open = stack.pop()
+            if (last_open, char) not in pairs:
+                return False
+    return len(stack) == 0
 
-Bracket = namedtuple("Bracket", ["char", "position"])
 
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i))
-        elif next in ")]}":
-            if not opening_brackets_stack:
-                return i + 1
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
-                return i + 1
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
-    else:
-        return "Success"
-
-def main():
-    choice = input("Enter 'F' to input a file, or 'I' to input brackets: ")
-    if choice == "F":
-        filename = input("Enter the filename: ")
-        if not os.path.exists(filename):
-            print(f"File '{filename}' does not exist.")
-            return
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
         with open(filename) as f:
-            text = f.read()
-    elif choice == "I":
-        text = input("Enter the brackets: ")
+            for line in f:
+                line = line.strip()
+                print('Success' if is_balanced(line) else 'Failure')
     else:
-        print("Invalid choice")
-        return
-
-    mismatch = find_mismatch(text)
-    print(mismatch)
-
-if __name__ == "__main__":
-    main()
+        s = input().strip()
+        print('Success' if is_balanced(s) else 'Failure')
